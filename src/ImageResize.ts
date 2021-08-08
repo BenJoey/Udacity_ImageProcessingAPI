ce899925ca9ece04f88c14f542fec634f316d9c2
Bence <bauerbence5@gmail.com>
2021-08-08 22:27:39 +0200

    Separeted image resize into another file

---
+++ src/ImageResize.ts
@@ -0,0 +1,22 @@
+import Jimp from 'jimp';
+import path from 'path';
+
+const ImageResize = async (
+  width: number,
+  height: number,
+  filename: string
+): Promise<{ success: boolean; result: string }> => {
+  try {
+    const image = await Jimp.read(
+      path.join('./assets', 'full', filename + '.jpg')
+    );
+    const newFileName = `${filename}_${width}_${height}.${image.getExtension()}`;
+    const resizedImage = image.resize(width, height, Jimp.RESIZE_BEZIER);
+    await resizedImage.writeAsync(path.join('./assets', 'thumb', newFileName));
+    return { success: true, result: newFileName };
+  } catch (err) {
+    return { success: false, result: err };
+  }
+};
+
+export default ImageResize;
--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,25 +1,19 @@
 import express from 'express';
-import Jimp from 'jimp';
+import ImageResize from '../../ImageResize';
 import path from 'path';
 
 const images = express.Router();
 
-images.get('/', (req, res) => {
+images.get('/', async (req, res) => {
   const width = parseInt(req.query.width as unknown as string);
   const height = parseInt(req.query.height as unknown as string);
   const filename = req.query.filename as unknown as string;
-  Jimp.read(path.join('./assets', 'full', filename + '.jpg'))
-    .then((image) => {
-      const newFileName = `${filename}_${width}_${height}.${image.getExtension()}`;
-      image
-        .resize(width, height, Jimp.RESIZE_BEZIER)
-        .write(path.join('./assets', 'thumb', newFileName), () => {
-          res.sendFile(newFileName, { root: path.join('./assets', 'thumb') });
-        });
-    })
-    .catch((err) => {
-      res.send('Error during image creation: ' + err);
-    });
+  const resizeOp = await ImageResize(width, height, filename);
+  if (resizeOp.success) {
+    res.sendFile(resizeOp.result, { root: path.join('./assets', 'thumb') });
+  } else {
+    res.send(resizeOp.result);
+  }
 });
 
 export default images;
