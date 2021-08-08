b80c38f1a5499dcf4afd97f2b0554607cba5cf79
Bence <bauerbence5@gmail.com>
2021-08-08 14:51:29 +0200

    Small refactor

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -8,15 +8,14 @@ images.get('/', (req, res) => {
   const width = parseInt(req.query.width as unknown as string);
   const height = parseInt(req.query.height as unknown as string);
   const filename = req.query.filename as unknown as string;
-  const fname = path.join('./assets', 'full', filename + '.jpg');
   Jimp.read(path.join('./assets', 'full', filename + '.jpg'))
     .then((image) => {
       const newFileName = `${filename}_${width}_${height}.${image.getExtension()}`;
-      console.log('image read ' + fname + ',   ' + newFileName);
-      const converted = image.resize(width, height, Jimp.RESIZE_BEZIER);
-      converted.write(path.join('./assets', 'thumb', newFileName), () => {
-        res.sendFile(newFileName, { root: './assets/thumb' });
-      });
+      image
+        .resize(width, height, Jimp.RESIZE_BEZIER)
+        .write(path.join('./assets', 'thumb', newFileName), () => {
+          res.sendFile(newFileName, { root: path.join('./assets', 'thumb') });
+        });
     })
     .catch((err) => {
       res.send('Error during image creation: ' + err);
