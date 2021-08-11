aed62f593ad353b7be81e5e25340b1a542ee3e22
Bence <bauerbence5@gmail.com>
2021-08-11 12:38:46 +0200

    Fancier request validator

--- src/middlewares/requestValidator.ts
+++ src/middlewares/requestValidator.ts
@@ -5,13 +5,13 @@ const requestValidator = (
   res: express.Response,
   next: express.NextFunction
 ) => {
-  console.log('InValidator');
-  let errMsg: String = '';
-  if (req.query.filename == undefined) errMsg += 'Missing filename; ';
-  if (req.query.width == undefined) errMsg += 'Missing image width; ';
-  if (req.query.height == undefined) errMsg += 'Missing image height; ';
-  if (errMsg) {
-    res.status(400).send(errMsg);
+  console.log('In Request Validator');
+  let errMsg: String[] = [];
+  if (req.query.filename == undefined) errMsg.push('Missing filename');
+  if (req.query.width == undefined) errMsg.push('Missing image width');
+  if (req.query.height == undefined) errMsg.push('Missing image height');
+  if (errMsg.length != 0) {
+    res.status(400).send(errMsg.join('; '));
   } else {
     next();
   }
